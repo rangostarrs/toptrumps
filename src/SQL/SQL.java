e42711478b241d264ff16569d2627117d0b628f5
Reiko <2417046l@bo720-9-06u>
2019-01-24 11:35:09 +0000

    Added SQL package and class.

--- src/SQL/SQL.java
+++ src/SQL/SQL.java
@@ -1,5 +1,43 @@
 package SQL;
-
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 public class SQL {
+	
 
-}
+	//Written by Reiko
+	
+		public static void main(String[] args) {
+		/// load the JDBC Driver 
+		try {
+			Class.forName("org.postgresql.Driver");
+		}catch(ClassNotFoundException e) {
+			System.out.println("Could not find JBDC Driver");
+			e.printStackTrace();
+			return;
+		}
+		System.out.println("PostgreSQL JDBC Driver Found!");
+		
+		Connection c = null;
+		
+		try {
+			//Toby's database information
+			c = DriverManager.getConnection("jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/", "m_18_2420282r", "2420282r");
+			
+			
+		}catch(SQLException e) {
+			System.out.println("Connection Failed!");
+			e.printStackTrace();
+			return;
+		}//Connection is completed
+		
+		if(c!=null) {
+			
+		}
+		
+	}
+	}
+
