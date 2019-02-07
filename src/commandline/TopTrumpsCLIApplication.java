b7ae7f980d38913751adca55cebd6103c21858b6
Monika Nizio <2424127N@student.gla.ac.uk>
2019-02-07 18:06:45 +0000

    Optimised and removed unnecessary code Added comments and integer restrictions

--- src/commandline/TopTrumpsCLIApplication.java
+++ src/commandline/TopTrumpsCLIApplication.java
@@ -9,7 +9,6 @@ import java.sql.Statement;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
-
 /**
  * Top Trumps command line application
  */
@@ -43,91 +42,92 @@ public class TopTrumpsCLIApplication {
 			// ----------------------------------------------------
 
 			userWantsToQuit = true; // use this when the user wants to exit the game
-				int x = model.gameIntro();
-				
-				model.addCardsToList();
-				if(x==1) {
-					/// load the JDBC Driver 
-					try {
-						   Class.forName("org.postgresql.Driver");
-						}
-						catch(ClassNotFoundException ex) {
-						   System.out.println("Error: unable to load driver class!");
-						   System.exit(1);
-						}
-				System.out.println("PostgreSQL JDBC Driver Found!");
-				
-				Connection c = null;
-					try {
+			int x = model.gameIntro();
 
-					c = DriverManager.getConnection("jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/", "m_18_2417046l", "2417046l");
+			model.addCardsToList();
+			if (x == 1) {
+				/// load the JDBC Driver
+				try {
+					Class.forName("org.postgresql.Driver");
+				} catch (ClassNotFoundException ex) {
+					System.out.println("Error: unable to load driver class!");
+					System.exit(1);
+				}
+				System.out.println("PostgreSQL JDBC Driver Found!");
+
+				Connection c = null;
+				try {
+
+					c = DriverManager.getConnection("jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/", "m_18_2417046l",
+							"2417046l");
 					System.out.println("Connection succeeded");
-					Statement stmt = c.createStatement();         
-			        ResultSet rs = stmt.executeQuery("Select Max (gameid) \r \n" +
-					 "From game;");
-					
-			        System.out.println("The history of game statistics is the following:");
-			        
-			         while ( rs.next() ) {             
-			        	 int gameid = rs.getInt(1);
-			        	 System.out.println("There has been " + gameid + " games in total.");}
-			         rs.close();
-			         stmt.close();
-			         Statement stm2 = c.createStatement();
-			         ResultSet rs2 = stm2.executeQuery("Select Count (winner) \r \n" + "From Game \r \n" + "Where winner LIKE 'cpu%';");
-			         while(rs2.next()) {
-			        	 int c_wins = rs2.getInt(1);
-			        	 System.out.println("CPU has won " + c_wins + " times.");
-			         }
-			         rs2.close();
-			         stm2.close();
-			         Statement stm3 = c.createStatement();
-			         ResultSet rs3 = stm3.executeQuery("Select Count (winner) \r \n" + "From Game \r \n" + "Where winner LIKE 'p%';");
-			         
-			         while(rs3.next()) {
-			        	 int h_wins = rs3.getInt(1);
-			        	 System.out.println("Humans have won " + h_wins + " times.");
-			         }
-			         rs3.close();
-			         stm3.close();
-			         Statement stm4 = c.createStatement();
-			         ResultSet rs4 = stm4.executeQuery("Select Avg(draws) \r \n" + "From Game;");
-			         
-			         while(rs4.next()) {
-			        	 double avg_draws = rs4.getDouble(1);
-			        	 NumberFormat formatter = new DecimalFormat("#0");
-			        	 System.out.println("There has been approximately " + formatter.format(avg_draws) + " draws per game.");
-			        	 
-			         }
-			         rs4.close();
-			         stm4.close();
-			         Statement stm5 = c.createStatement();
-			         ResultSet rs5 = stm5.executeQuery("Select Max(totalrounds) \r \n" + "From Game;");
-			         
-			         while(rs5.next()) {
-			        	 int highest_totalrounds = rs5.getInt(1);
-			        	 System.out.println("The highest number of rounds played in any of the games has been " + highest_totalrounds + ".");
-			         }
-			         rs5.close();
-			         stm5.close();
-			         
-			         Statement stm6 = c.createStatement();
-			         
-			         stm6.executeUpdate("INSERT INTO Game " + "VALUES (9, 'player', 45)");
-			         
-			         stm6.close();
-			         
-			         c.close();
-			         c = null;
-					
-				}catch(SQLException e) {
+					Statement stmt = c.createStatement();
+					ResultSet rs = stmt.executeQuery("Select Max (gameid) \r \n" + "From game;");
+
+					System.out.println("The history of game statistics is the following:");
+
+					while (rs.next()) {
+						int gameid = rs.getInt(1);
+						System.out.println("There has been " + gameid + " games in total.");
+					}
+					rs.close();
+					stmt.close();
+					Statement stm2 = c.createStatement();
+					ResultSet rs2 = stm2.executeQuery(
+							"Select Count (winner) \r \n" + "From Game \r \n" + "Where winner LIKE 'cpu%';");
+					while (rs2.next()) {
+						int c_wins = rs2.getInt(1);
+						System.out.println("CPU has won " + c_wins + " times.");
+					}
+					rs2.close();
+					stm2.close();
+					Statement stm3 = c.createStatement();
+					ResultSet rs3 = stm3.executeQuery(
+							"Select Count (winner) \r \n" + "From Game \r \n" + "Where winner LIKE 'p%';");
+
+					while (rs3.next()) {
+						int h_wins = rs3.getInt(1);
+						System.out.println("Humans have won " + h_wins + " times.");
+					}
+					rs3.close();
+					stm3.close();
+					Statement stm4 = c.createStatement();
+					ResultSet rs4 = stm4.executeQuery("Select Avg(draws) \r \n" + "From Game;");
+
+					while (rs4.next()) {
+						double avg_draws = rs4.getDouble(1);
+						NumberFormat formatter = new DecimalFormat("#0");
+						System.out.println(
+								"There has been approximately " + formatter.format(avg_draws) + " draws per game.");
+
+					}
+					rs4.close();
+					stm4.close();
+					Statement stm5 = c.createStatement();
+					ResultSet rs5 = stm5.executeQuery("Select Max(totalrounds) \r \n" + "From Game;");
+
+					while (rs5.next()) {
+						int highest_totalrounds = rs5.getInt(1);
+						System.out.println("The highest number of rounds played in any of the games has been "
+								+ highest_totalrounds + ".");
+					}
+					rs5.close();
+					stm5.close();
+
+					Statement stm6 = c.createStatement();
+
+					stm6.executeUpdate("INSERT INTO Game " + "VALUES (9, 'player', 45)");
+
+					stm6.close();
+
+					c.close();
+					c = null;
+
+				} catch (SQLException e) {
 					System.out.println("Connection Failed22!");
 					e.printStackTrace();
 					return;
-				}//Connection is completed
-				
-				
-				
+				} // Connection is completed
 
 // 			try {
 // 				TopTrumpsController.startGame();
@@ -137,10 +137,9 @@ public class TopTrumpsCLIApplication {
 // 				e.printStackTrace();
 // 			}
 
+			}
 
 		}
 
 	}
-
-}
 }
--- src/commandline/TopTrumpsModel.java
+++ src/commandline/TopTrumpsModel.java
@@ -15,13 +15,13 @@ import SQL.SQL;
 
 //--------------------------------
 //When we deal cards, we add cards at the TOP of the deque (addFirst() method)
-//When we take a card from the top of our stack, we we use pollFirst()
+//When we take a card from the top of our stack, we use pollFirst()
 //When we put cards on the bottom of our stack, we use addLast()
 //--------------------------------
 public class TopTrumpsModel {
 
 	private ArrayList<Card> cardList = new ArrayList<Card>();
-	private String headerArray[] = new String[5];
+	private static String headerArray[] = new String[6];
 	private Deque<Card> mainDeck = new ArrayDeque<Card>();
 	private Deque<Card> playerDeck = new ArrayDeque<Card>();
 	private Deque<Card> cpu1Deck = new ArrayDeque<Card>();
@@ -34,15 +34,16 @@ public class TopTrumpsModel {
 	// common pile used in case of a draw
 	private ArrayList<Card> commonPile = new ArrayList<Card>();
 	private int drawNumber = 0;
-//	private int playerRoundWin = 0;
-//	private int cpu1RoundWin = 0;
-//	private int cpu2RoundWin = 0;
-//	private int cpu3RoundWin = 0;
-//	private int cpu4RoundWin = 0;
-	//player with the highest stat, initially 0
+	private int playerRoundWin = 0;
+	private int cpu1RoundWin = 0;
+	private int cpu2RoundWin = 0;
+	private int cpu3RoundWin = 0;
+	private int cpu4RoundWin = 0;
+	// player with the highest stat, initially 0
 	private int highestStatPlayer = 0;
-	//THIS IS CURRENTLY HARDCODED FOR 4 AI PLAYERS:
-	private int[] gameResults = {0, 0, 0, 0, 0};
+	private int[] gameResults = { 0, 0, 0, 0, 0 };
+	private int gameID;
+	private int roundNumber = 1;
 
 	TopTrumpsModel() {
 
@@ -57,14 +58,27 @@ public class TopTrumpsModel {
 				+ "Do you want to see past results or play a game?\n" + "   1: Print Game Statistics\n"
 				+ "   2: Play game\n");
 
-		int selection = getInt("Enter the number for your selection:", userInput);
+		int selection = 0;
+		// number of opponents
+		int cpuNumber = 1;
+		while (true) {
+			selection = getInt("Enter the number for your selection:", userInput);
+			if (selection == 1) {
+				// code for the database
+			} else if (selection == 2) {
+				while (true) {
+					cpuNumber = getInt("Choose number of oppononents (1-4)", userInput);
+					if (cpuNumber > 0 && cpuNumber < 5) {
+						break;
+					}
+				}
 
-		if (selection == 1) {
-
-		} else if (selection == 2) {
-			int cpuNumber = getInt("Choose number of oppononents", userInput);
-			addCardsToList();
-			gameLoop(cpuNumber, cardList);
+				addCardsToList();
+				gameLoop(cpuNumber, cardList);
+				break;
+			} else {
+				System.out.println("Please enter '1' to print game statistics or '2' to play game");
+			}
 		}
 		return selection;
 	}
@@ -72,90 +86,71 @@ public class TopTrumpsModel {
 	public GameStats gameLoop(int cpuNumber, ArrayList<Card> cardList) {
 
 		Scanner userInput = new Scanner(System.in);
-		int roundNumber = 1;
-		//stat that will be played in the given round
+
+		// stat that will be played in the given round
 		int statSelection = 0;
 
 		dealCards(cpuNumber, cardList);
-		
 
-		//REDUNDANT CODE? <ignore>:
-		// find where in the ArrayList the player is after shuffling so he can be
-		// accessed
-		// int playerArrayPos = findPlayerPosition(playersList);
-		
 		System.out.println("Game Start");
 
 		while (true) {
 
-			//if this value = 0, it means that the player starts the round
+			// if this value = 0, it means that the player starts the round
 			int playerArrayPos = 1;
-			
-			//reset variable
+
+			// reset variable
 			highestStatPlayer = 0;
-			
+
 			System.out.println("\n \n Round " + roundNumber + "\n______________ ");
 			System.out.println("\n Round " + roundNumber + ": Players have drawn their cards");
 
-			//if player was not eliminated from the game, do this:
-			if(!playerDeck.isEmpty()) {
-				//check position of the player in the array (if 0, he starts the round)
+			// if player was not eliminated from the game, do this:
+			if (!playerDeck.isEmpty()) {
+				// check position of the player in the array (if 0, he starts the round)
 				playerArrayPos = playersList.indexOf(player);
-				//peek first card
+				// peek first card
 				Card currentCard = playersList.get(playerArrayPos).getDeck().peekFirst();
-
-				//display drawn card
-				System.out.println("You drew " + currentCard.toString());
-				System.out.println("There are " + playersList.get(playerArrayPos).getDeck().size() + " cards in your deck");
+				// display drawn card
+				System.out.println("\n You drew " + currentCard.toString());
+				// display how many cards are left in the player's deck (without the currently
+				// drawn card)
+				System.out.println(
+						"There are " + (playersList.get(playerArrayPos).getDeck().size() - 1) + " cards in your deck");
 			}
-			
+			// play card
+			statSelection = currentPlayerMove(playerArrayPos, statSelection, userInput);
 
-			// check who's the current player - user or cpu
-			try {
-				//THERE WAS A LOGIC ERROR IN CARD CLASS RETURNING HIGHEST STAT -
-				//IT WAS RETURNING VALUE OF THE STAT, NOT STAT INDEX -
-				//try-catch NOT NEEDED ANYMORE?
-				statSelection = currentPlayerMove(playerArrayPos, statSelection, userInput);
-			} catch (NoSuchElementException e) {
-
-				System.out.println("Doesn't Exist");
-
-			}
-
-
-			
 			// collect hands (first card from each player)
 			collectCurrentHands();
-//			try {
-				// compare stat between the players
-				highestStatPlayer = compareStat(statSelection);
-//			} catch (IndexOutOfBoundsException e) {
-//
-//				System.out.println("Too Big");
-//			}
+
+			// compare stat between the players
+			highestStatPlayer = compareStat(statSelection);
+
 			// <draw>
 			if (highestStatPlayer == 6) {
+				// add hands to the common pile in case of a draw
 				for (int i = 0; i < currentHands.size(); i++) {
 					commonPile.add(currentHands.get(i));
 				}
-
 				// clear hands
 				currentHands.clear();
 
-				System.out.println("DRAW \n");
+				System.out.println("DRAW \nStat chosen was: " + headerArray[statSelection]);
 				drawNumber++;
-				
-				checkPlayerEliminated(playerArrayPos);
 
+				checkPlayerEliminated(playerArrayPos);
 			}
 			// </end of draw>
 			else {
-				System.out
-						.println("Round " + roundNumber + " won by: " + playersList.get(highestStatPlayer).toString());
+				System.out.println(
+						"\nRound " + roundNumber + " won by: " + playersList.get(highestStatPlayer).toString());
 				// ADD AN ARROW INDICATING WINNING STAT:
-				System.out.println("The winning card was: \n" + currentHands.get(highestStatPlayer).toString());
+				System.out.println("\nThe winning card was: " + currentHands.get(highestStatPlayer).toString());
+				System.out.println("The winning stat was: " + headerArray[statSelection] + ": "
+						+ currentHands.get(highestStatPlayer).returnStat(statSelection));
 
-				// GIVE CARDS TO THE WINNING PLAYER
+				// give hands to the winner
 				giveHandsToWinner(highestStatPlayer);
 
 				// check if any decks are empty
@@ -165,91 +160,62 @@ public class TopTrumpsModel {
 				reorderPlayersList(playersList, highestStatPlayer);
 
 				// increase number of wins for the current player
-				//PLACEHOLDER METHOD
-				if (playersList.get(highestStatPlayer).toString() == "Human") {
-					gameResults[0] = gameResults[0] + 1;
-				}
-				else if (playersList.get(highestStatPlayer).toString() == "Opponent 1") {
-					gameResults[1] = gameResults[1] + 1;
-				}
-				
-				else if(playersList.get(highestStatPlayer).toString() == "Opponent 2") {
-					gameResults[2] = gameResults[2] + 1;
-				}
-				
-				else if(playersList.get(highestStatPlayer).toString() == "Opponent 3") {
-					gameResults[3] = gameResults[3] + 1;
-				}
-				
-				else if(playersList.get(highestStatPlayer).toString() == "Opponent 4") {
-					gameResults[4] = gameResults[4] + 1;
-				}
-				
+				increaseRoundWin(highestStatPlayer);
 			}
 
-			// in case of win:
+			// finish the game if only 1 player left
 			if (playersList.size() == 1) {
 				System.out.println("Game End");
 				break;
 			}
 
-			//alternative winning condition - check if any player has over 40 cards
-//			for (int i=0;i>playersList.size();i++) {
-//				if(playersList.get(i).getDeck().size() >= 40) {
-//					break;
-//				}
-//			}
-			
-			
 			// stop the game at the end of the round
 			pressAnyKeyToContinue();
 
 			// move on to the next round
 			roundNumber++;
 
-			//summary of the common pile where cards are put in case of a draw
+			// summary of the common pile where cards are put in case of a draw
 			System.out.println("Common pile: " + commonPile.size());
 		}
-		
+
 		// print out how many games the player won
-			System.out.println("The overall winner was " + playersList.get(0).toString() +
-			"\n Scores: "
-			+ "\n Human: " + gameResults[0] + 
-			 "\n Opponent 1: " + gameResults[1] + 
-			 "\n Opponent 2: " + gameResults[2] + 
-			 "\n Opponent 3: " + gameResults[3] + 
-			 "\n Opponent 4: " + gameResults[4]);
+		System.out.println("The overall winner was " + playersList.get(0).toString() + "\n Scores: " + "\n Human: "
+				+ gameResults[0] + "\n Opponent 1: " + gameResults[1] + "\n Opponent 2: " + gameResults[2]
+				+ "\n Opponent 3: " + gameResults[3] + "\n Opponent 4: " + gameResults[4]);
 
-
-		// game end (placeholder):
-		GameStats gameStats = new GameStats(0, playersList.get(0).toString(), roundNumber, gameResults[0], gameResults[1], gameResults[2],
-				gameResults[3], gameResults[4], drawNumber);
+		// game end:
+		GameStats gameStats = new GameStats(gameID, playersList.get(0).toString(), roundNumber, playerRoundWin,
+				cpu1RoundWin, cpu2RoundWin, cpu3RoundWin, cpu4RoundWin, drawNumber);
 		return gameStats;
-
-		
 
 	}
 
-	public void increaseRoundWinStat(int winner) {
-		// CHANGE - PLAYER IS NOT ALWAYS ON POSITION 1
-//
-//		switch (winner) {
-//		case 0:
-//			playerRoundWin++;
-//			break;
-//		case 1:
-//			cpu1RoundWin++;
-//			break;
-//		case 2:
-//			cpu2RoundWin++;
-//			break;
-//		case 3:
-//			cpu3RoundWin++;
-//			break;
-//		case 4:
-//			cpu4RoundWin++;
-//			break;
-//		}
+	public void increaseRoundWin(int highestStatPlayer) {
+
+		if (playersList.get(highestStatPlayer).toString() == "Human") {
+			gameResults[0] = gameResults[0] + 1;
+			playerRoundWin++;
+
+		} else if (playersList.get(highestStatPlayer).toString() == "Opponent 1") {
+			gameResults[1] = gameResults[1] + 1;
+			cpu1RoundWin++;
+		}
+
+		else if (playersList.get(highestStatPlayer).toString() == "Opponent 2") {
+			gameResults[2] = gameResults[2] + 1;
+			cpu2RoundWin++;
+		}
+
+		else if (playersList.get(highestStatPlayer).toString() == "Opponent 3") {
+			gameResults[3] = gameResults[3] + 1;
+			cpu3RoundWin++;
+		}
+
+		else if (playersList.get(highestStatPlayer).toString() == "Opponent 4") {
+			gameResults[4] = gameResults[4] + 1;
+			cpu4RoundWin++;
+		}
 	}
 
 	public void reorderPlayersList(ArrayList<Player> playersList, int highestStatPlayer) {
@@ -259,96 +225,79 @@ public class TopTrumpsModel {
 	public void giveHandsToWinner(int highestStatPlayer) {
 		for (int i = 0; i < currentHands.size(); i++) {
 			playersList.get(highestStatPlayer).getDeck().addLast(currentHands.get(i));
-			
+
 		}
 		if (!commonPile.isEmpty()) {
 			for (int i = 0; i < commonPile.size(); i++) {
 				playersList.get(highestStatPlayer).getDeck().addLast(commonPile.get(i));
 			}
 		}
-		System.out.println("the winner now has "+playersList.get(highestStatPlayer).getDeck().size()+" cards");
+		System.out.println("\nThe winner now has " + playersList.get(highestStatPlayer).getDeck().size() + " cards \n");
 		currentHands.clear();
 		commonPile.clear();
 	}
 
 	public int compareStat(int statSelection) {
+		// return index of the player with the highest score
 		int highestStatPlayer = 0;
-//		System.out.println("Debugging: value of the chosen stat for the player on position 0: "
-//				+ currentHands.get(0).returnStat(statSelection));
 		for (int i = 1; i < currentHands.size(); i++) {
-//			System.out.println("Debugging: value of the chosen stat for the player on position " + i + ": "
-//					+ currentHands.get(i).returnStat(statSelection));
 			if (currentHands.get(i).returnStat(statSelection) > currentHands.get(highestStatPlayer)
 					.returnStat(statSelection)) {
 				highestStatPlayer = i;
-
-			} 
+			}
 		}
-			for (int i = 1; i < currentHands.size(); i++)
-			{
-				if(i == highestStatPlayer) {
-					//skip this index
-				}
-				else if (currentHands.get(i).returnStat(statSelection) == currentHands.get(highestStatPlayer)
+		for (int i = 1; i < currentHands.size(); i++) {
+			if (i == highestStatPlayer) {
+				// skip this index
+			} else if (currentHands.get(i).returnStat(statSelection) == currentHands.get(highestStatPlayer)
 					.returnStat(statSelection)) {
 				// if draw, set highestStatPlayer to 6 (max value of i is 5)
 				highestStatPlayer = 6;
 				break;
 			}
 		}
-		System.out.println("The highest stat player is: " + highestStatPlayer );
 		return highestStatPlayer;
 	}
 
 	public void collectCurrentHands() {
 
-		//int statComparisonArray[] = new int[playersList.size()];
-		
 		for (int i = 0; i < playersList.size(); i++) {
-
 			// retrieve hand of each player
-
-			// playersList.get(i).getDeck().peekFirst().returnStat(statSelection);
 			Card currentHandCard = playersList.get(i).getDeck().pollFirst();
 			currentHands.add(currentHandCard);
-			 
+
 		}
-
-		System.out.println("Current hand size =" +currentHands.size());
-		//return statComparisonArray;
-
 	}
 
 	public int currentPlayerMove(int playerArrayPos, int statSelection, Scanner userInput) {
 		// if user, go to [USER MOVE]
 		if (playerArrayPos == 0) {
 			// [USER MOVE]
-			System.out.println("It is your turn to select a category, the categories are:" + "\n 1: " + headerArray[0]
-					+ "\n 2: " + headerArray[1] + "\n 3: " + headerArray[2] + "\n 4: " + headerArray[3] + "\n 5: "
-					+ headerArray[4]);
-			statSelection = getInt("Enter the number for your attribute:", userInput);
-			
+			System.out.println("It is your turn to select a category, the categories are:" + "\n 1: " + headerArray[1]
+					+ "\n 2: " + headerArray[2] + "\n 3: " + headerArray[3] + "\n 4: " + headerArray[4] + "\n 5: "
+					+ headerArray[5]);
+			while (true) {
+				statSelection = getInt("Enter the number for your attribute (1-5):", userInput);
+				if (statSelection > 0 && statSelection < 6) {
+					break;
+				}
+			}
+
 		}
 		// [CPU MOVE]
 		else {
 			statSelection = cpuPlayCard(playersList, 0);
-			System.out.println("Cpu Stat Selection = "+ statSelection); 
 		}
 		return statSelection;
 	}
 
 	public void checkPlayerEliminated(int playerPos) {
-		//NOT SURE IF playersList size is verified on each iteration?
 		for (int i = 0; i < playersList.size(); i++) {
 			if (playersList.get(i).getDeck().isEmpty()) {
-//				if (i == playerPos  && playersList.size() > 1) {
-//					System.out.println("You have been eliminated from the game");
-//				} else {
-					System.out.println(playersList.get(i).toString() + " was eliminated");
-//				}
-				//remove players without cards from the playersList
+				System.out.println(playersList.get(i).toString() + " was eliminated");
+				// remove player without cards from the playersList
 				playersList.remove(i);
-				System.out.println("player list size = "+ playersList.size());
+				System.out.println("player list size = " + playersList.size());
 				i--;
 			}
 		}
@@ -357,11 +306,11 @@ public class TopTrumpsModel {
 	public int cpuPlayCard(ArrayList<Player> playersList, int pos) {
 		Card currentCpuCard = playersList.get(pos).getDeck().peekFirst();
 		int cpuStatChoice = currentCpuCard.returnHighestStat(currentCpuCard);
-		System.out.println("the computer stat = " + cpuStatChoice);
 		return cpuStatChoice;
 	}
 
 	public int findPlayerPosition(ArrayList<Player> playersList) {
+		// find position of the player in the playerArrayList
 		int playerPos = 0;
 		for (int i = 0; i < playersList.size(); i++) {
 			if (playersList.get(i) == player) {
@@ -372,8 +321,6 @@ public class TopTrumpsModel {
 	}
 
 	public ArrayList<Player> createPlayersArray(int cpuNumber) {
-
-		// ArrayList<Player> playersList = new ArrayList<Player>();
 
 		playersList.add(player);
 		Player cpu1 = new Player("Opponent 1", cpu1Deck);
@@ -399,11 +346,7 @@ public class TopTrumpsModel {
 	public void dealCards(int cpuNumber, ArrayList<Card> cardList) {
 
 		mainDeck = shuffleCards(cardList);
-		//System.out.println("Main deck" + mainDeck.toString());
 		while (!mainDeck.isEmpty()) {
-			if (mainDeck.isEmpty()) {
-				break;
-			}
 			playerDeck.addFirst(mainDeck.pollFirst());
 			if (mainDeck.isEmpty()) {
 				break;
@@ -431,14 +374,7 @@ public class TopTrumpsModel {
 				}
 			}
 		}
-		System.out.println("Player deck" + playerDeck.toString());
-		System.out.println("deck 1:" + cpu1Deck.toString());
-		System.out.println("deck 2:" + cpu2Deck.toString());
-		System.out.println("deck 3:" + cpu3Deck.toString());
-		System.out.println("deck 4:" + cpu4Deck.toString());
-
 		createPlayersArray(cpuNumber);
-
 	}
 
 	public Deque<Card> shuffleCards(ArrayList<Card> cardList) {
@@ -461,8 +397,8 @@ public class TopTrumpsModel {
 			String line = scanner.nextLine();
 			String[] tokens = line.split(" ");
 
-			for (int i = 0; i < 5; i++) {
-				headerArray[i] = tokens[i + 1];
+			for (int i = 0; i < 6; i++) {
+				headerArray[i] = tokens[i];
 			}
 
 			while (scanner.hasNextLine()) {
@@ -470,20 +406,19 @@ public class TopTrumpsModel {
 				tokens = line.split(" ");
 
 				String description = tokens[0];
-				int size = Integer.parseInt(tokens[1]);
-				int speed = Integer.parseInt(tokens[2]);
-				int range = Integer.parseInt(tokens[3]);
-				int firepower = Integer.parseInt(tokens[4]);
-				int cargo = Integer.parseInt(tokens[5]);
+				int stat1 = Integer.parseInt(tokens[1]);
+				int stat2 = Integer.parseInt(tokens[2]);
+				int stat3 = Integer.parseInt(tokens[3]);
+				int stat4 = Integer.parseInt(tokens[4]);
+				int stat5 = Integer.parseInt(tokens[5]);
 
-				Card cardObject = new Card(description, size, speed, range, firepower, cargo);
+				Card cardObject = new Card(description, stat1, stat2, stat3, stat4, stat5);
 				cardList.add(cardObject);
 			}
 
 			scanner.close();
 			String listOfCards = cardList.toString();
-			// System.out.println(listOfCards.replace("[", "").replace("]",
-			// "").replaceAll(",", ""));
+
 		} catch (FileNotFoundException exception) {
 			exception.printStackTrace();
 		} finally {
@@ -512,9 +447,13 @@ public class TopTrumpsModel {
 
 	private void pressAnyKeyToContinue() {
 		System.out.println("Press \"ENTER\" to continue...");
-		   Scanner sc = new Scanner(System.in);
-		   sc.nextLine();
-		   
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine();
+
+	}
+
+	public static String[] getHeaderArray() {
+		return headerArray;
 	}
 
 }
