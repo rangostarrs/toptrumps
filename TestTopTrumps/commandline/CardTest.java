7b5c697a4e4c92eb81ff76f27dc34537c6c419db
Nick's Account <Nick's Account@172.30.135.6>
2019-02-15 18:16:53 +0000

    Added a new test

---
+++ TestTopTrumps/commandline/CardTest.java
@@ -0,0 +1,40 @@
+package commandline;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class CardTest {
+
+	
+	private Card cardyCard = new Card ("Lovely Card", 4, 2, 8, 6, 7);
+	
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void returnHighestStattest() {
+		
+		
+		//checking that the index returned by the highest stat method is the same as on our
+		//example card
+		assertEquals(2,cardyCard.returnHighestStat(cardyCard));
+		
+	}
+	
+	@Test
+	void returnStatTest() {
+		
+		int statIndex = 4;
+		assertEquals(6,cardyCard.returnStat(statIndex));
+		
+		
+		
+	}
+
+}
